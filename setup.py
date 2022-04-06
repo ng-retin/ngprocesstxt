5ae0500d11d1e67d286a06367f03a8b22e0553b0
ng-retin <retin.kumar@ngenux.com>
2022-04-07 01:32:58 +0530

    updates

--- setup.py
+++ setup.py
@@ -3,24 +3,24 @@ from setuptools import find_packages, setup
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="ngclntxt",
+setup(
+    name="ngprocesstxt",
     version=0.1,
     author="Retin",
     description="A small text cleaning package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ng-retin/ngclntxt",
+    url="https://github.com/ng-retin/ngprocesstxt",
     project_urls={
-        "Bug Tracker": "https://github.com/ng-retin/ngclntxt/issues",
+        "Bug Tracker": "https://github.com/ng-retin/ngprocesstxt/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "ngprocesstxt"},
-    packages=find_packages(where="ngprocesstxt/"),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     python_requires=">=3.6",
-    download_url="https://github.com/ng-retin/ngclntxt/archive/refs/tags/0.1.tar.gz"
+    download_url="https://github.com/ng-retin/ngprocesstxt/archive/refs/tags/0.1.tar.gz"
 )
